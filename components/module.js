99e6a2f995aeeeac73fee3d58f9e84af3c8bf363
Daniel Shumway <shumway.danny@gmail.com>
2014-06-01 14:32:08 -0400

    Created a base module.js file for us to use when creating components from now.  In the process of converting everything over.
    Signed-off-by: Daniel Shumway <shumway.danny@gmail.com>

---
+++ components/module.js
@@ -0,0 +1,168 @@
+//module.js
+
+/*
+-- Module serves as the base object for almost everything else in the game.
+-- It can be overridden and extended, but it provides a backbone for what we're doing.
+*/
+
+function Module(_x, _y, _width, _height){
+
+	//------------INIT---------------------------
+	//These variables will be copied into toReturn.
+	//THEY SHOULD NOT BE USED INTERNALLY
+	//-------------------------------------------
+
+	//ToDo: Allow for null values.
+	_bounds = {"x": _x, "y":_y, "width":_width, "height":_height, };
+
+	_loaded = true; //We'll set this to false when necessary.
+	_contents = []; //Sub-modules.
+
+	//-----------INTERNAL VARIABLES------------------------
+
+	//None for now.
+
+	//------------LOADING EVENTS-------------------
+
+
+	var _fireOnLoad = undefined;
+	var _contextForLoad = undefined;
+	function _onSubLoad() {
+		for(var i = 0; i<toReturn.contents.length; i++){
+			if(!toReturn.contents[i].loaded) { toReturn.loaded = false; return; }//Don't trigger.
+		} //Got through?
+		if(_onLoaded) {
+			toReturn.loaded = true;
+			if(_contextOnLoaded) { _onLoaded(_contextOnLoaded); } else { _onLoaded(this); }
+		}
+	}
+
+	function _setLoad(_function, _ctx) {
+		_onLoaded = _function; _contextOnLoaded = _ctx;
+	}
+
+	//--------------------------------------------
+
+	function _addModule(_object) {
+		//There's a heck of a lot more that needs to go
+		//in this method.  I think.
+		_object.setLoad(_onSubLoad); //Add it to loading queue.
+		toReturn.contents.push(_object);
+
+		//If it's already loaded, fire event in response
+		//and tell us if manager is loaded.
+		if(_object.loaded) _onSubLoad();
+	}
+
+	//----------------------------------------------
+
+
+	function _update(dx){
+		//Update sub-modules.
+		for(var i =0; i<toReturn.contents.length; i++){
+			toReturn.contents[i].update(dx);
+		}
+	}
+
+
+	function _draw(){
+		var toDraw = [];
+		//Recursively get all sprite data to draw.
+		for(var i=0; i<toReturn.contents.length; i++){
+			toDraw = toDraw.concat(toReturn.contents[i].draw());
+		}
+
+		//Set offsets.
+		for(i=0; i<toDraw.length; i++){
+			toDraw[i].x += toReturn.interface.bounds.x;
+			toDraw[i].y += toReturn.interface.bounds.y;
+		}
+
+		return toDraw;
+	}
+
+
+	//-----------------------------------------------------------------------------
+	//---------------------PUBLIC INTERFACE----------------------------------------
+	//-----------------------------------------------------------------------------
+
+
+	//ToDo: fill in blank documentation.
+	var toReturn = {
+
+		/*-------------------------------------------------------------------------
+		--These variables will be exposed to the extended class, but no one else.
+		--Use them internally, otherwise, leave them alone.
+		--------------------------------------------------------------------------*/
+
+		//An array of sub-modules stored within this module.
+		"contents":_contents,
+
+		//Returns an array of sprite information to draw.
+		//Return the results of this method from your draw method.
+		"draw":_draw,
+
+		//
+		/*
+
+		*/
+		"addModule":_addModule,
+
+
+		//Sets a function to be fired off when the module has finished loading.
+		/*
+		function: the function to be called, ctx: the object to set the context to (optional)
+		*/
+		"setLoad":_setLoad,
+
+		//Update sub-modules.
+		/*
+		dx: the amount of time, in miliseconds, since the last update.
+		*/
+		"update":_update,
+
+
+		/*------------------------------------------------------------------------
+		--These variables are made available as a default interface you can return.  
+		--You can make your own, if you're confident you know how, or (better option)
+		--just append onto this one, replace methods as required, then return it modified.
+		--------------------------------------------------------------------------*/
+
+
+		"interface": {
+
+			//Object with the dimensions and positions of the module.
+			//{x, y, width, height}
+			"bounds": _bounds,
+
+			//Boolean of whether or not the module is loaded.  
+			//False by default, should be set to true while initializing.
+			"loaded": _loaded,
+
+			//Updates the module and all sub-modules.
+			/*
+			dx: the amount of time, in miliseconds, since the last update.
+			*/
+			"update":_update,
+
+			//Returns an array of sprite information to draw.
+			"draw":_draw,
+
+			//Sets a function to be fired off when the module has finished loading.
+			/*
+			function: the function to be called, ctx: the object to set the context to (optional)
+			*/
+			"setLoad":_setLoad,
+
+			//
+			/*
+
+			*/
+			"addModule":_addModule,
+
+		},
+
+	};
+
+	return toReturn;
+}
--- components/playArea.js
+++ components/playArea.js
@@ -11,62 +11,18 @@
 function PlayArea(_x, _y, _width, _height) {
 
 
-	//Private variables and internal functions.
 	//------------------------------VARIABLES-------------------------------------
 
-	var x = _x;
-	var y = _y; 
-	var width = _width; 
-	var height = _height; 
+	var base = Module(_x, _y, _width, _height); //Call base
+	var toReturn = base.interface; //Set toReturn via base.
+	toReturn.draw = _draw; //Modify public interface.
 
-	var _contents = []; //Images and parts in the playArea.
+	//---------------------------FUNCTIONS---------------------------------------
 
-
-	//-----------------Loading event-----------------------
-
-	var _fireOnLoad = undefined;
-	var _contextForLoad = undefined;
-	function _onSubLoad() {
-		for(var i = 0; i<_contents.length; i++){
-			if(!_contents[i].loaded) { toReturn.loaded = false; return; }//Don't trigger.
-		} //Got through?
-		if(_onLoaded) {
-			toReturn.loaded = true;
-			if(_contextOnLoaded) { _onLoaded(_contextOnLoaded); } else { _onLoaded(this); }
-		}
-	}
-
-	function _setLoad(_function, _ctx) {
-		_onLoaded = _function, _contextOnLoaded = _ctx;
-
-	}
-
-	//---------------------------------------------------
-
-	function _addGameObject(_object) {
-		//There's a heck of a lot more that needs to go
-		//in this method.  I think.
-		_object.setLoad(_onSubLoad); //Add it to loading queue.
-		_contents.push(_object);
-
-		//If it's already loaded, fire event in response
-		//and tell us if manager is loaded.
-		if(_object.loaded) _onSubLoad();
-	}
 
 	function _draw(){
 
-		var toDraw = [];
-		for(var i=0; i<_contents.length; i++){
-			toDraw = toDraw.concat(_contents[i].draw());
-		}
-
-		//Set offsets.
-		for(var i=0; i<toDraw.length; i++){
-			toDraw[i].x += toReturn.bounds.x;
-			toDraw[i].y += toReturn.bounds.y;
-		}
-
+		toDraw = base.draw();
 
 		//Temp Dev Test
 		// Red rectangle
@@ -75,40 +31,13 @@ function PlayArea(_x, _y, _width, _height) {
 		ctx.beginPath();
 		ctx.lineWidth="10";
 		ctx.strokeStyle="red";
-		ctx.rect(x,y,width,height);
+		ctx.rect(toReturn.bounds.x, toReturn.bounds.y, toReturn.bounds.width, toReturn.bounds.height);
 		ctx.stroke();
 		//------------------End testing environment-------------------------------------
 		//------------------------------------------------------------------------------
-		
+
 		return toDraw;
-
 	}	
-
-
-	//Public interface.
-	var toReturn = {
-
-		//Public variables.  We don't need methods for these.
-		"bounds":{"x":0, "y":0, "width":1920, "height":1080 },
-
-		"loaded":false /*should be true?*/,
-
-		//Set a function to be fired off when the sprite has finished loading.
-		/*
-		function: the function to be called, ctx: the object to set the context to (optional)
-		*/
-		"setLoad":_setLoad,
-
-		//adds a gameObject
-		/*
-		gameObject: object to Add - note that gameObject refers to the wrapper around Sprite for our dolls/characters/whatever.
-		It is *not* a universal object, and should *not* be used for logos, buttons, etc...
-		*/
-		"addGameObject":_addGameObject,
-
-		//Returns an array of sprites to draw.
-		"draw":_draw,
-	}
 
 	return toReturn;
 }
