1888a6618f988f85c1f4937a2563fa6d5c3c6599
Nick Buonarota <Nickydab@gmail.com>
2014-06-24 11:58:57 -0400

    Nick Tues Sprint Complete. Working toward full tech demo. Added all pages for screens in app.

---
+++ components/Screen.js
@@ -0,0 +1,39 @@
+
+
+
+
+function Screen(_x, _y, _width, _height){
+	//------------------------------VARIABLES-------------------------------------
+	var base = Module(_x, _y, _width, _height); //Call base
+	var toReturn = base.interface; //Set toReturn via base.
+
+	//Modify public interface and add variables as necessary.
+	toReturn.draw = _draw;
+	Touch.Collisions(base);
+	
+	//-------FOR DEV PURPOSES
+	var x = _x;
+	var y = _y; 
+	var width = _width; 
+	var height = _height; 
+	
+	var _fireOnLoad = undefined;
+	var _contextForLoad = undefined;
+	
+	//------------------------------FUNCTIONS-------------------------------------
+	function _draw(){
+
+		var toDraw = base.draw();
+		// Blue rectangle
+		ctx.beginPath();
+		ctx.lineWidth="10";
+		ctx.strokeStyle="blue";
+		ctx.rect(x,y,width,height);
+		ctx.stroke();
+		//------------------End testing environment code--------------------------------
+		//------------------------------------------------------------------------------
+		
+		return toDraw;
+	}
+	return toReturn;
+}
