4ad466d5c789750ec21f2a73502c3485e5df644f
Daniel Shumway <shumway.danny@gmail.com>
2014-06-30 10:24:57 -0400

    Finished grid and have an example usage in the main screen (power selection)
    Signed-off-by: Daniel Shumway <shumway.danny@gmail.com>

--- components/grid.js
+++ components/grid.js
@@ -11,6 +11,10 @@ function Grid(_x, _y, _rows, _columns, _itemWidth, _itemHeight, _margin) {
 	var width = _columns * (_margin + _itemWidth);
 	var height = _rows * (_margin + _itemHeight)
 	var itemWidth = _itemWidth;
+	var itemHeight = _itemHeight;
+	var margin = _margin;
+	var rows = _rows;
+	var columns = _columns
 
 	//Set up base.
 	var base = Module(_x, _y, width, height);
@@ -30,7 +34,7 @@ function Grid(_x, _y, _rows, _columns, _itemWidth, _itemHeight, _margin) {
 	/*
 
 	*/
-	toReturn.setSpace = function(toSet, column, row){
+	toReturn.setSpace = function(toSet, column, row, matchSize){
 		//If it's already there, remove the module.
 		if(_grid[column][row] !== undefined) {
 			base.removeModule(_grid[clolumn][row]);
@@ -39,10 +43,20 @@ function Grid(_x, _y, _rows, _columns, _itemWidth, _itemHeight, _margin) {
 		_grid[column][row] = toSet;
 		if(toSet !== undefined) {
 			base.addModule(toSet);
-			//Set it to correct position and width.
-			toSet.bounds.width = 
+
+			//Set it to correct position and width, if we're meant to do that.
+			if(matchSize) {
+				toSet.bounds.width = itemWidth;
+				toSet.bounds.height = itemHeight;
+			}
+			//Regardless, set to right position.
+			toSet.bounds.x = (itemWidth + margin)*column;
+			toSet.bounds.y = (itemWidth + margin)*row;
 		}
 	}
+
+	//ToDo: Add additional controls preventing people from setting bounds (internal checks to see if anything has been changed)
+	//Add controls for people to get more information out.
 
 
 	return toReturn;
