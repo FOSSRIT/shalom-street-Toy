b3f5faa5eeee987e41041ecd6d58559667427413
Daniel Shumway <shumway.danny@gmail.com>
2014-07-01 10:04:01 -0400

    CharacterBioScreen is added.

--- components/superhero/characterBioScreen.js
+++ components/superhero/characterBioScreen.js
@@ -1,46 +1,9 @@
-function PowersScreen(_info){
+function CharacterBioScreen(_info){
 	//------------------------------VARIABLES-------------------------------------
 	var base = State(0, 0, 1920, 1080); //Call base
 	var toReturn = base.interface; //Set toReturn via base.
-	toReturn.type = "PowersScreen";
+	toReturn.type = "CharacterBioScreen";
 	var info = _info;
-
-
-	superhero.powers = [];
-				
-	//Dev Splash Image
-	splashImage = Sprite(0,0,1920,1080, "images/dev/powersScreenTest01.png");
-	base.addModule(splashImage);
-	
-	//Buttons
-	backButton = Sprite(0, 1080-128, 128, 128, "images/dev/back.png");
-	base.addModule(backButton);
-	
-	continueButton = Sprite(1920-128, 1080-128, 128, 128, "images/dev/continue.png");
-	base.addModule(continueButton);
-	
-	quitButton = Sprite(0, 0, 128, 128, "images/dev/quit.png");
-	base.addModule(quitButton);
-	
-	//Powers
-	power1Button = Sprite(toReturn.bounds.width/5, toReturn.bounds.height/3-128, 256, 256, "images/dev/power1_unselected.png");
-	base.addModule(power1Button);
-	
-	power2Button = Sprite(toReturn.bounds.width/5, 2*toReturn.bounds.height/3-128, 256, 256, "images/dev/power2_unselected.png");
-	base.addModule(power2Button);
-	
-	//Events
-	backButton.addEvent("mousedown", base.changeState("CharacterBuilder", _info), false);
-	continueButton.addEvent("mousedown", base.changeState("CharacterBioScreen", _info), false);
-	quitButton.addEvent("mousedown", base.changeState("SplashScreen", _info), false);
-	
-	power1Button.addEvent("mousedown", function(_clipBoard){
-		highlightQualityAndPowerButtons("powers",power1Button,"saveKitten","images/dev/power1_unselected.png","images/dev/power1_selected.png", _clipBoard);
-	},false);
-	
-	power2Button.addEvent("mousedown", function(_clipBoard){
-		highlightQualityAndPowerButtons("powers",power2Button,"notSaveKitten","images/dev/power2_unselected.png","images/dev/power2_selected.png", _clipBoard);
-	},false);
 
 
 	//--------------------------------------
@@ -67,20 +30,10 @@ function PowersScreen(_info){
 	base.addModule(quitButton);
 	
 	//Events
-	backButton.addEvent("mousedown", function(_clipBoard){
-		manager.removeModule(sc);
-		switchState("powersScreen");
-	},false);
-	
-	continueButton.addEvent("mousedown", function(_clipBoard){
-		manager.removeModule(sc);
-		switchState("resultsScreen");
-	},false);
-	
-	quitButton.addEvent("mousedown", function(_clipBoard){
-		manager.removeModule(sc);
-		switchState("splashScreen");
-	},false);
+	backButton.addEvent("mousedown", base.changeState("PowersScreen", _info), false);
+	continueButton.addEvent("mousedown", base.changeState("ResultsScreen", _info), false);
+	quitButton.addEvent("mousedown", base.changeState("SplashScreen", _info), false);
+
 
 
 
