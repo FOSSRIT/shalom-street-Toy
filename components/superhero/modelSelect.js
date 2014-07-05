db22946960761cde7660f718b02e073a40fc66b1
Daniel Shumway <shumway.danny@gmail.com>
2014-07-05 11:39:54 -0400

    Fixed two quick errors that I missed with last commit.
    Signed-off-by: Daniel Shumway <shumway.danny@gmail.com>

--- components/superhero/modelSelect.js
+++ components/superhero/modelSelect.js
@@ -51,8 +51,8 @@ function ModelSelect(_info){
 	base.addModule(quitButton);
 	
 	//Events
-	backButton.addEvent("mousedown", base.changeState("CharacterBuilder", _info), false);
-	continueButton.addEvent("mousedown", base.changeState("ResultsScreen", _info), false);
+	backButton.addEvent("mousedown", base.changeState("PowersScreen", _info), false);
+	continueButton.addEvent("mousedown", base.changeState("CharacterBuilder", _info), false);
 	quitButton.addEvent("mousedown", base.changeState("SplashScreen", _info), false);
 
 
