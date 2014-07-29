7cb856ec76d0deb05a9531a68a7b83151105c6ea
Nick Buonarota <Nickydab@gmail.com>
2014-07-29 11:56:36 -0400

    powers page: now has drop down menus

--- superhero/powersScreen.js
+++ superhero/powersScreen.js
@@ -25,6 +25,8 @@ function PowersScreen(_info){
 	base.addModule(heroImage);
 	*/
 	
+	
+	
 	var definition = Sprite(3*toReturn.bounds.width/4-128,toReturn.bounds.height/2-256,512,512, "images/dev/midrash_water.png");
 	base.addModule(definition);
 	
@@ -40,52 +42,172 @@ function PowersScreen(_info){
 	
 	//Powers
 	
-	var cat1_element_button = Sprite(64, 6*toReturn.bounds.height/16, 256, 64, "images/dev/qualitiesButtons/brave.png");
+	var cat1_element_button = Sprite(64, 6*toReturn.bounds.height/16, 256, 64, "images/dev/powersButtons/elements.png");
 	base.addModule(cat1_element_button);
 	
-	/*
-	var power3_fire_button = Sprite(0, 2*toReturn.bounds.height/8, 256, 128, "images/dev/powersButtons/fire.png");
-	base.addModule(power3_fire_button);
+	var cat2_energy_button = Sprite(64, 9*toReturn.bounds.height/16, 256, 64, "images/dev/powersButtons/energy.png");
+	base.addModule(cat2_energy_button);
 	
-	var power4_water_button = Sprite(0, 3*toReturn.bounds.height/8, 256, 128, "images/dev/powersButtons/water.png");
-	base.addModule(power4_water_button);
+	var cat3_mutant_button = Sprite(64, 10*toReturn.bounds.height/16, 256, 64, "images/dev/powersButtons/mutant.png");
+	base.addModule(cat3_mutant_button);
 	
-	var power5_fly_button = Sprite(0, 4*toReturn.bounds.height/8, 256, 128, "images/dev/powersButtons/fly.png");
-	base.addModule(power5_fly_button);
+	var cat4_super_button = Sprite(64, 11*toReturn.bounds.height/16, 256, 64, "images/dev/powersButtons/super.png");
+	base.addModule(cat4_super_button);
 	
-	var power6_diplomacy_button = Sprite(0, 5*toReturn.bounds.height/8, 256, 128, "images/dev/powersButtons/diplomacy.png");
-	base.addModule(power6_diplomacy_button);
+	//ToyboxTab
+	var elementContent = ToyBoxTab(64,7*toReturn.bounds.height/16,256,128, "powers");
+	base.addModule(elementContent);
 	
-	var power7_superSmart_button = Sprite(0, 6*toReturn.bounds.height/8, 256, 128, "images/dev/powersButtons/superSmart.png");
-	base.addModule(power7_superSmart_button);
-	*/
+	var energyContent = ToyBoxTab(64,8*toReturn.bounds.height/16,256,128, "powers");
+	base.addModule(energyContent);
+	
+	var mutantContent = ToyBoxTab(64,9*toReturn.bounds.height/16,256,256, "powers");
+	base.addModule(mutantContent);
+	
+	var superContent = ToyBoxTab(64,10*toReturn.bounds.height/16,256,256, "powers");
+	base.addModule(superContent);
+	
+	//content powers
+	//element
+	var fireButton = Sprite(0, 0, 256, 64, "images/dev/powersButtons/fire.png");
+	elementContent.addModule(fireButton);
+	var waterButton = Sprite(0, 64, 256, 64, "images/dev/powersButtons/water.png");
+	elementContent.addModule(waterButton);
+	//energy
+	var healButton = Sprite(0, 0, 256, 64, "images/dev/powersButtons/fire.png");
+	energyContent.addModule(healButton);
+	var laserButton = Sprite(0, 64, 256, 64, "images/dev/powersButtons/water.png");
+	energyContent.addModule(laserButton);
+	//mutant
+	var duplicate = Sprite(0, 0, 256, 64, "images/dev/powersButtons/fire.png");
+	mutantContent.addModule(duplicate);
+	var mind = Sprite(0, 64, 256, 64, "images/dev/powersButtons/water.png");
+	mutantContent.addModule(mind);
+	var teleport = Sprite(0, 128, 256, 64, "images/dev/powersButtons/fire.png");
+	mutantContent.addModule(teleport);
+	var xray = Sprite(0, 192, 256, 64, "images/dev/powersButtons/water.png");
+	mutantContent.addModule(xray);
+	//super
+	var flight = Sprite(0, 0, 256, 64, "images/dev/powersButtons/fire.png");
+	superContent.addModule(flight);
+	var speed = Sprite(0, 64, 256, 64, "images/dev/powersButtons/water.png");
+	superContent.addModule(speed);
+	var smart = Sprite(0, 128, 256, 64, "images/dev/powersButtons/fire.png");
+	superContent.addModule(smart);
+	var strength = Sprite(0, 192, 256, 64, "images/dev/powersButtons/water.png");
+	superContent.addModule(strength);
+	
+	//prep for screen
+	var selectedCategory = elementContent;
+	elementContent.select();
 	
 	//Events
 	backButton.addEvent("mousedown", base.changeState("QualitiesScreen", _info), false);
 	continueButton.addEvent("mousedown", base.changeState("GenderSelect", _info), false);
 	quitButton.addEvent("mousedown", base.changeState("SplashScreen", _info), false);
 	
-	/*
-	power3_fire_button.addEvent("mousedown", function(_clipBoard){
-		highlightQualityAndPowerButtons("powers",power3_fire_button,"fire","images/dev/powersButtons/fire.png","images/dev/powersButtons/fire_selected.png", _clipBoard);
-	},false);
+	//Swap content Events
+	cat1_element_button.addEvent("mousedown", function(_clipBoard){
+
+		//If we need to unselect things.
+		if(selectedCategory != undefined) {
+			cat1_element_button.setImage("images/dev/powersButtons/elements.png"); //Unselect
+			selectedCategory.deselect();
+		}
+		
+		//Set our own image.  And mark that we're selected.
+		selectedCategory = elementContent;
+		cat1_element_button.setImage("images/dev/powersButtons/elements.png");
+		//And load in all of our tabs.
+		elementContent.select();
+		
+		//reset pos of tabs
+		cat2_energy_button.setImage("images/dev/powersButtons/energy.png", true, 64, 9*toReturn.bounds.height/16, 256, 64); 
+		cat3_mutant_button.setImage("images/dev/powersButtons/mutant.png", true, 64, 10*toReturn.bounds.height/16, 256, 64); 
+		cat4_super_button.setImage("images/dev/powersButtons/super.png", true, 64, 11*toReturn.bounds.height/16, 256, 64); 
+
+		if(_clipBoard.ToFire){
+			_clipBoard.ToFire.push("redraw");
+		} else {
+			_clipBoard.ToFire = ["redraw"];
+		}
+
+	}, false);
 	
-	power4_water_button.addEvent("mousedown", function(_clipBoard){
-		highlightQualityAndPowerButtons("powers",power4_water_button,"water","images/dev/powersButtons/water.png","images/dev/powersButtons/water_selected.png", _clipBoard);
-	},false);
+	cat2_energy_button.addEvent("mousedown", function(_clipBoard){
+
+		//If we need to unselect things.
+		if(selectedCategory != undefined) {
+			cat2_energy_button.setImage("images/dev/powersButtons/energy.png", true, 64, 7*toReturn.bounds.height/16, 256, 64); //Unselect
+			selectedCategory.deselect();
+		}
+		
+		//Set our own image.  And mark that we're selected.
+		selectedCategory = energyContent;
+		cat2_energy_button.setImage("images/dev/powersButtons/energy.png");
+		//And load in all of our tabs.
+		energyContent.select();
+		
+		cat3_mutant_button.setImage("images/dev/powersButtons/mutant.png", true, 64, 10*toReturn.bounds.height/16, 256, 64); 
+		cat4_super_button.setImage("images/dev/powersButtons/super.png", true, 64, 11*toReturn.bounds.height/16, 256, 64); 
+
+		if(_clipBoard.ToFire){
+			_clipBoard.ToFire.push("redraw");
+		} else {
+			_clipBoard.ToFire = ["redraw"];
+		}
+
+	}, false);
 	
-	power5_fly_button.addEvent("mousedown", function(_clipBoard){
-		highlightQualityAndPowerButtons("powers",power5_fly_button,"fly","images/dev/powersButtons/fly.png","images/dev/powersButtons/fly_selected.png", _clipBoard);
-	},false);
+	cat3_mutant_button.addEvent("mousedown", function(_clipBoard){
+
+		//If we need to unselect things.
+		if(selectedCategory != undefined) {
+			cat3_mutant_button.setImage("images/dev/powersButtons/mutant.png", true, 64, 8*toReturn.bounds.height/16, 256, 64); //Unselect
+			selectedCategory.deselect();
+		}
+		
+		//Set our own image.  And mark that we're selected.
+		selectedCategory = mutantContent;
+		cat3_mutant_button.setImage("images/dev/powersButtons/mutant.png");
+		//And load in all of our tabs.
+		mutantContent.select();
+		
+		cat2_energy_button.setImage("images/dev/powersButtons/energy.png", true, 64, 7*toReturn.bounds.height/16, 256, 64); 
+		cat4_super_button.setImage("images/dev/powersButtons/super.png", true, 64, 13*toReturn.bounds.height/16, 256, 64); 
+
+		if(_clipBoard.ToFire){
+			_clipBoard.ToFire.push("redraw");
+		} else {
+			_clipBoard.ToFire = ["redraw"];
+		}
+
+	}, false);
 	
-	power6_diplomacy_button.addEvent("mousedown", function(_clipBoard){
-		highlightQualityAndPowerButtons("powers",power6_diplomacy_button,"diplomacy","images/dev/powersButtons/diplomacy.png","images/dev/powersButtons/diplomacy_selected.png", _clipBoard);
-	},false);
-	
-	power7_superSmart_button.addEvent("mousedown", function(_clipBoard){
-		highlightQualityAndPowerButtons("powers",power7_superSmart_button,"superSmart","images/dev/powersButtons/superSmart.png","images/dev/powersButtons/superSmart_selected.png", _clipBoard);
-	},false);
-	*/
+	cat4_super_button.addEvent("mousedown", function(_clipBoard){
+
+		//If we need to unselect things.
+		if(selectedCategory != undefined) {
+			cat4_super_button.setImage("images/dev/powersButtons/super.png", true, 64, 9*toReturn.bounds.height/16, 256, 64); //Unselect
+			selectedCategory.deselect();
+		}
+		
+		//Set our own image.  And mark that we're selected.
+		selectedCategory = superContent;
+		cat4_super_button.setImage("images/dev/powersButtons/super.png");
+		//And load in all of our tabs.
+		superContent.select();
+		
+		cat2_energy_button.setImage("images/dev/powersButtons/energy.png", true, 64, 7*toReturn.bounds.height/16, 256, 64); 
+		cat3_mutant_button.setImage("images/dev/powersButtons/mutant.png", true, 64, 8*toReturn.bounds.height/16, 256, 64);
+
+		if(_clipBoard.ToFire){
+			_clipBoard.ToFire.push("redraw");
+		} else {
+			_clipBoard.ToFire = ["redraw"];
+		}
+
+	}, false);
 	
 	
 
